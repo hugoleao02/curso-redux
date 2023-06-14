118f685de4a14dc9201d7dbae6134e222f34100e
Hugo <h.ferreira147@gmail.com>
2023-06-13 22:09:49 -0300

    mymoney

---
+++ myMoneyApp/myMoney-app/src/test/java/com/example/myMoneyapp/MyMoneyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myMoneyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoneyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
